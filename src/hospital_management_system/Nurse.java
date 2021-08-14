1545bcadda58615f02e5d0a9e3d9fe50ca577ab1
AKASH PAL <palakash680@gmail.com>
2021-08-14 17:30:06 +0530

    HMS_pt1

---
+++ src/hospital_management_system/Nurse.java
@@ -0,0 +1,13 @@
+package hospital_management_system;
+
+public class Nurse extends Staff{
+    private long nurseId;
+
+    public long getNurseId() {
+        return nurseId;
+    }
+
+    public void setNurseId(long nurseId) {
+        this.nurseId = nurseId;
+    }
+}
